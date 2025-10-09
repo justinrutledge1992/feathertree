70d37b5eca5de1ac5fffc10dbd2733d0647afaf0
Justin Rutledge <justin@jrutledge.net>
2025-10-09 18:35:07 -0400

    added STATICFILES_DIRS

--- feathertree_project/settings.py
+++ feathertree_project/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Uncomment the line below if you have extra static files and a directory in your GitHub repo.
 # If you don't have this directory and have this uncommented your build will fail
